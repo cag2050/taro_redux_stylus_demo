a17620539aacd28b8a81a20c002ba78db13ac449
chen_anguo <cag2050@163.com>
2018-11-09 10:14:42 +0800

    m

--- src/app.js
+++ src/app.js
@@ -11,8 +11,9 @@ import './app.styl'
 const store = configStore()
 
 class App extends Component {
-
+  // 通常入口文件会包含一个 config 配置项，这里的配置主要参考微信小程序的全局配置而来，在编译成小程序时，这一部分配置将会被抽离成 app.json，而编译成其他端，亦会有其他作用。
   config = {
+    // 页面创建好后如果需要渲染展示，则需要在项目入口文件 app.js 中 config 的 pages 数组中进行指定，例如 index 页面，需要如下进行配置，页面配置需要指定到页面具体的 js 文件，可以不带 .js 后缀
     pages: [
       'pages/index/index'
     ],
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,4 @@
+// 必须声明 Taro 和组件:https://nervjs.github.io/taro/docs/jsx.html#%E5%BF%85%E9%A1%BB%E5%A3%B0%E6%98%8E-taro-%E5%92%8C%E7%BB%84%E4%BB%B6
 import Taro, {Component} from '@tarojs/taro'
 import {View, Button, Text} from '@tarojs/components'
 import {connect} from '@tarojs/redux'
@@ -22,7 +23,7 @@ import './index.styl'
 }))
 
 class Index extends Component {
-
+  // Taro 的页面同样是继承自 Component 组件基类，每一个页面都拥有自己配置 config，这个配置参考自微信小程序的页面配置，在编译成小程序时，将会生成跟页面 JS 文件同名的 json 配置文件；在编译成 H5 时，config 配置中 navigationBarTitleText 将会被用来设置当前页面的标题。
   config = {
     navigationBarTitleText: '首页'
   }
@@ -41,6 +42,7 @@ class Index extends Component {
   componentDidHide() {
   }
 
+  // 页面 JS 要求必须有一个 render 函数，函数返回 JSX 代码
   render() {
     return (
       <View className='index'>
